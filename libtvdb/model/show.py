eaa1f5592efd7aceb56e45e8c664911e8cfeea93
Dale Myers <dale@myers.io>
2018-12-21 16:32:59 +0000

    Use deserialize to parse Show

--- libtvdb/model/show.py
+++ libtvdb/model/show.py
@@ -3,9 +3,67 @@
 import datetime
 from typing import Any, Dict, List, Optional
 
+import deserialize
+
 from libtvdb.model.enums import AirDay, ShowStatus
 from libtvdb.utilities import parse_date, parse_datetime, require, try_pop
 
+
+def date_parser(value: Optional[str]) -> Optional[datetime.datetime]:
+    """Parser method for parsing dates to pass to deserialize."""
+    if value is None:
+        return None
+
+    if value == '0000-00-00' or value == '':
+        return None
+
+    return parse_date(value)
+
+
+def datetime_parser(value: Optional[str]) -> Optional[datetime.datetime]:
+    """Parser method for parsing datetimes to pass to deserialize."""
+    if value is None:
+        return None
+
+    if value == '0000-00-00 00:00:00' or value == '':
+        return None
+
+    return parse_datetime(value)
+
+
+def timestamp_parser(value: Optional[int]) -> Optional[datetime.datetime]:
+    """Parser method for parsing datetimes to pass to deserialize."""
+    if value is None:
+        return None
+
+    return datetime.datetime.fromtimestamp(value)
+
+
+def status_parser(value: Optional[str]) -> str:
+    if value is None or value == '':
+        return ShowStatus.unknown.value
+
+    return value
+
+
+@deserialize.key("identifier", "id")
+@deserialize.key("name", "seriesName")
+@deserialize.key("first_aired", "firstAired")
+@deserialize.key("series_identifier", "seriesId")
+@deserialize.key("network_identifier", "networkId")
+@deserialize.key("genres", "genre")
+@deserialize.key("last_updated", "lastUpdated")
+@deserialize.key("air_day", "airsDayOfWeek")
+@deserialize.key("air_time", "airsTime")
+@deserialize.key("imdb_id", "imdbId")
+@deserialize.key("zap2it_id", "zap2itId")
+@deserialize.key("added_by", "addedBy")
+@deserialize.key("site_rating", "siteRating")
+@deserialize.key("site_rating_count", "siteRatingCount")
+@deserialize.parser("status", status_parser)
+@deserialize.parser("firstAired", date_parser)
+@deserialize.parser("lastUpdated", timestamp_parser)
+@deserialize.parser("added", datetime_parser)
 class Show:
     """Represents a single show."""
 
@@ -34,162 +92,3 @@ class Show:
     added_by: Optional[int]
     site_rating: Optional[float]
     site_rating_count: Optional[int]
-
-
-    def __init__(
-            self,
-            *,
-            identifier: int,
-            name: str,
-            slug: str,
-            status: ShowStatus,
-            aliases: List[str],
-            network: str,
-            first_aired: Optional[datetime.date] = None,
-            overview: Optional[str] = None,
-            banner: Optional[str] = None,
-            series_identifier: Optional[str] = None,
-            network_identifier: Optional[str] = None,
-            runtime: Optional[str] = None,
-            genres: Optional[List[str]] = None,
-            last_updated: Optional[datetime.datetime] = None,
-            air_day: Optional[AirDay] = None,
-            air_time: Optional[str] = None,
-            rating: Optional[str] = None,
-            imdb_id: Optional[str] = None,
-            zap2it_id: Optional[str] = None,
-            added: Optional[datetime.datetime] = None,
-            added_by: Optional[int] = None,
-            site_rating: Optional[float] = None,
-            site_rating_count: Optional[int] = None
-        ) -> None:
-
-        self.identifier = identifier
-        self.name = name
-        self.slug = slug
-        self.status = status
-        self.first_aired = first_aired
-        self.aliases = aliases
-        self.network = network
-        self.overview = overview
-        self.banner = banner
-
-        self.series_identifier = series_identifier
-        self.network_identifier = network_identifier
-        self.runtime = runtime
-        self.genres = genres
-        self.last_updated = last_updated
-        self.air_day = air_day
-        self.air_time = air_time
-        self.rating = rating
-        self.imdb_id = imdb_id
-        self.zap2it_id = zap2it_id
-        self.added = added
-        self.added_by = added_by
-        self.site_rating = site_rating
-        self.site_rating_count = site_rating_count
-
-        if self.identifier is None:
-            raise ValueError("Identifier should not be None")
-
-        if self.name is None:
-            raise ValueError("Name should not be None")
-
-        if self.slug is None:
-            raise ValueError("Slug should not be None")
-
-        if self.status is None:
-            raise ValueError("Status should not be None")
-
-        if self.aliases is None:
-            raise ValueError("Aliases should not be None")
-
-        if self.network is None:
-            raise ValueError("Network should not be None")
-
-    def __str__(self):
-        return f"{self.name} ({self.identifier})"
-
-    @staticmethod
-    def from_json(data: Dict[str, Any]) -> 'Show':
-        """Convert Show data from the API to a Show object."""
-
-        print(data)
-
-        # Get the fields which are always there
-        aliases = require(try_pop(data, 'aliases'))
-        first_aired_string = try_pop(data, 'firstAired')
-        identifier = require(try_pop(data, 'id'))
-        network = require(try_pop(data, 'network'))
-        overview = try_pop(data, 'overview')
-        series_name = require(try_pop(data, 'seriesName'))
-        slug = require(try_pop(data, 'slug'))
-        status_string = try_pop(data, 'status')
-        banner = try_pop(data, 'banner')
-
-        # These fields are only there on a specific show load (i.e. not a search)
-        series_identifier = try_pop(data, 'seriesId')
-        network_identifier = try_pop(data, 'networkId')
-        runtime = try_pop(data, 'runtime')
-        genres = try_pop(data, 'genre')
-        last_updated_stamp = try_pop(data, 'lastUpdated')
-        air_day_string = try_pop(data, 'airsDayOfWeek')
-        air_time = try_pop(data, 'airsTime')
-        rating = try_pop(data, 'rating')
-        imdb_id = try_pop(data, 'imdbId')
-        zap2it_id = try_pop(data, 'zap2itId')
-        added_date_string = try_pop(data, 'added')
-        added_by = try_pop(data, 'addedBy')
-        site_rating = try_pop(data, 'siteRating')
-        site_rating_count = try_pop(data, 'siteRatingCount')
-
-        if len(data.keys()) > 0:
-            raise Exception(f"Extra keys remain in Show data: {data.keys()}")
-
-        if status_string is None or status_string == "":
-            status = ShowStatus.unknown
-        else:
-            status = ShowStatus(status_string)
-
-        if first_aired_string is None or first_aired_string == "":
-            first_aired = None
-        else:
-            first_aired = parse_date(first_aired_string)
-
-        last_updated: Optional[datetime.datetime] = None
-        if last_updated_stamp is not None:
-            last_updated = datetime.datetime.fromtimestamp(last_updated_stamp)
-
-        added: Optional[datetime.datetime] = None
-        if added_date_string is not None and added_date_string != "":
-            added = parse_datetime(added_date_string)
-
-        air_day: Optional[AirDay] = None
-        if air_day_string is not None:
-            air_day = AirDay(air_day_string)
-
-        return Show(
-            identifier=identifier,
-            name=series_name,
-            aliases=aliases,
-            first_aired=first_aired,
-            network=network,
-            overview=overview,
-            slug=slug,
-            status=status,
-            banner=banner,
-            series_identifier=series_identifier,
-            network_identifier=network_identifier,
-            runtime=runtime,
-            genres=genres,
-            last_updated=last_updated,
-            air_day=air_day,
-            air_time=air_time,
-            rating=rating,
-            imdb_id=imdb_id,
-            zap2it_id=zap2it_id,
-            added=added,
-            added_by=added_by,
-            site_rating=site_rating,
-            site_rating_count=site_rating_count
-        )
